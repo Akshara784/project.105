fd03c22d36e34744b67c17643fa48092eae48c7a
Akshara784 <81739223+Akshara784@users.noreply.github.com>
2021-10-05 07:34:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera= document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function click(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML= '<img id="capture_image" src="'+data_uri+'"/>';
+
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/CZ7_4Oy1Jw/model.json', modelLoaded);
+
+function modelLoaded(){
+    console.log('model loaded!');
+}
+
+function Identify(){
+    img= document.getElementById('capture_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+
+    else{
+        console.log(results);
+        document.getElementById(result_person_name).innerHTML= results[0].label;
+        document.getElementById(result_person_accuracy).innerHTML= results[0].confidence.toFixed(2);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
